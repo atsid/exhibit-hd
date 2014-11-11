2fe3f42fa6eda43356c507da74fa4cf5066c480e
Jon McLean <jon.mclean@atsid.com>
2014-11-11 11:18:35 -0800

    Got the client closer to working.  Checking in current work before changing to the other zookeeper client.

--- app/registry-services.js
+++ app/registry-services.js
@@ -8,14 +8,45 @@ module.exports = function (app, config) {
     app.get('/registry', config.middleware, function (request, response, next) {
         console.log("return list of all schemas filtered by param type (type name is TBD)");
 
-        client.get('/objects', function(err, obj) {
-            response.send(obj);
+
+        client.readdir('/objects', function (err, nodes) {
+            assert.ifError(err);
+
+            var someObj = {};
+            var requiredCount = nodes.length;
+            var foundCount = 0;
+
+            for (var i = 0; i < nodes.length; i++) {
+                var id = nodes[i];
+                client.get('/objects/' + id, function(err, obj) {
+                    someObj[id] = obj;
+
+                    console.log("Wrining " + id);
+
+                    foundCount++;
+                    if(foundCount == requiredCount) {
+                        console.log("Writing response " + someObj);
+                        response.send(someObj);
+                    }
+                })
+            }
+            console.log("Exiting function");
+            //response.send(someObj);
+            //console.log(nodes); // => ['00000000', 'baz']
+            //response.send(someObj);
         });
+
+/*        client.get('/objects', function(err, obj) {
+            response.send(obj);
+        });*/
     });
 
     app.put('/registry/:id', config.middleware, function (request, response, next) {
         console.log("save schema with id " + request.params.id);
 
+        client.mkdirp('/objects', function(err) {
+            assert.ifError(err);
+        });
         client.put('/objects/' + request.params.id, request.body, function(err) {
             assert.ifError(err);
         });
